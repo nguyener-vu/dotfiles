2f597f77396261548aae6281ea2e5f0ee7e5ad88
Vu Nguyen <nguyenervu@gmail.com>
2023-07-30 08:22:15 -0700

    add nvim install on codespaces

--- run-postinstall.sh
+++ run-postinstall.sh
@@ -6,4 +6,15 @@ if [ -z $CODESPACES ]; then
 	./post-install.sh
 else
 	echo "Is in CODESPACES"
+  export ZPLUG_HOME=~/.zplug
+  git clone https://github.com/zplug/zplug $ZPLUG_HOME
+  apt-get update -y && apt-get upgrade -y
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  chmod u+x nvim.appimage
+  ./nvim.appimage --appimage-extract
+  ./squashfs-root/AppRun --version
+
+  # Optional: exposing nvim globally.
+  sudo mv squashfs-root /
+  sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 fi
