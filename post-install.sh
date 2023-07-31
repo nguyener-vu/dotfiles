88a45028cabdd197863082528439029468139d48
Vu Nguyen <nguyenervu@gmail.com>
2023-07-30 19:25:46 -0700

    update install based ubuntu

--- post-install.sh
+++ post-install.sh
@@ -23,6 +23,19 @@ echo "$OS detected"
 case $OS in
 'Linux')
 	echo "Linux"
+		curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+		chmod u+x nvim.appimage
+		./nvim.appimage --appimage-extract
+		./squashfs-root/AppRun --version
+
+		# Optional: exposing nvim globally.
+		sudo mv squashfs-root /
+		sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+    sudo apt install -y gcc g++
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
 	;;
 'FreeBSD')
 	echo "FreeBSD"
